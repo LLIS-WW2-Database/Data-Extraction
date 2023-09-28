daa54a8dac4ea48dbba246e35435e9ccba460e6f
Sebastian <sebastianmostert663@gmail.com>
2023-09-28 20:37:45 +0200

    Start.bat Fixes #2
    Added automatic installation of node dependencies to the start.bat file
    
    - Start.bat now runs npm i
    - Start.bat is more descriptive
    
    Closes #2

--- start.bat
+++ start.bat
@@ -1,2 +1,8 @@
 @echo off
+echo Installing dependencies...
+call npm install
+echo Installed dependencies.
+echo Running "start" function...
 node -e "require('./functions/generalFunctions').%1start();"
+echo The program has finished.
+pause
